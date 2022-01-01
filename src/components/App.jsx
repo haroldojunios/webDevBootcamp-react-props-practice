7bf55a215810fd29914120091cd643d19849884c
Haroldo Júnio <haroldojunios@gmail.com>
2022-01-01 18:16:29 -0300

    Added `Avatar` component inside `App`

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Card from "./Card";
+import Avatar from "./Avatar";
 import contacts from "../contacts";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar
+       img="https://thumbs.dreamstime.com/b/contacts-icon-communication-collection-contacts-icon-trendy-modern-flat-linear-vector-contacts-icon-white-background-130941596.jpg"
+       />
       {contacts.map((contact) =>
         <Card
           key={contact.name}
